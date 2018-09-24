fd24de0d4e2b533db5357819bbfbfa560a9e5f03
hongxuan <1055087560@qq.com>
2018-09-24 20:00:10 +0800

    首次添加工程gittest

---
+++ gittest/src/cn/itcast/test/Git_Test.java
@@ -0,0 +1,18 @@
+package cn.itcast.test;
+
+public class Git_Test {
+
+    public static void main(String[] args) {
+        System.out.println("66666666666");
+    }
+
+    public void test(){
+        int i=10;
+        i+=10;
+    }
+
+
+    public  void rt(){
+        System.out.println("safs");
+    }
+}
